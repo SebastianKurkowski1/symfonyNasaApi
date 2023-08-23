381e75a7cd892fd72ffb4ac03f491b4cda386711
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-08-23 23:20:49 +0200

    working jwt token generation

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -9,13 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(Request $request, RoverRepository $roverRepository): Response
+    public function homepage(Request $request, RoverRepository $roverRepository, HttpClientInterface $httpClient): Response
     {
-        dd($roverRepository->findAll());
         return $this->render('homepage/homepage.html.twig');
     }
 }
