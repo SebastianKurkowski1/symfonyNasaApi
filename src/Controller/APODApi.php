242db2afbc6dafa00814227f0cc802b3736481f9
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-08-28 00:58:24 +0200

    made api documentation with nelmio

--- src/Controller/APODApi.php
+++ src/Controller/APODApi.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\APOD;
 use App\Repository\APODRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Routing\Annotation\Route;
 
 class APODApi extends AbstractController
 {
+    #[OA\Tag('APOD')]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns astronomical picture of the day',
+        content: new Model(type: APOD::class),
+    )]
     #[Route('/api/apod/{date}', name: 'api_apod', methods: ['POST'])]
     public function getMRP(
         string        $date,
--- src/Controller/MRPApi.php
+++ src/Controller/MRPApi.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\MRP;
 use App\Repository\MRPRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 
 class MRPApi extends AbstractController
 {
+    #[OA\Tag('MRP')]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns array of Mars pictures from specified rover',
+        content: new Model(type: MRP::class)
+    )]
     #[Route('/api/mrp/{roverName}/{offset}', name: 'api_mrp_rover_and_offset', methods: ['POST'])]
     public function getMRPByRoverName(
         string        $roverName,
@@ -20,6 +29,13 @@ class MRPApi extends AbstractController
         return $this->json($MRPData);
     }
 
+
+    #[OA\Tag('MRP')]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns array of Mars pictures from specified date period',
+        content: new Model(type: MRP::class)
+    )]
     #[Route('/api/mrp/{dateFrom}/{dateTo}/{offset}', name: 'api_mrp_date_range', methods: ['POST'])]
     public function getMRPByDates(
         string        $dateFrom,
@@ -32,6 +48,12 @@ class MRPApi extends AbstractController
         return $this->json($MRPData);
     }
 
+    #[OA\Tag('MRP')]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns array of Mars pictures from specified rover and his date of mission',
+        content: new Model(type: MRP::class)
+    )]
     #[Route('/api/mrp-sol/{roverName}/{sol}', name: 'api_mrp_rover_and_sol', methods: ['POST'])]
     public function getMRPByRoverNameAndSol(
         string        $roverName,
