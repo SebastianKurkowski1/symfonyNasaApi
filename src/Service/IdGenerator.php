20c730074476634e1f22fb8dfee90bc1f71925f8
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-09-02 14:14:20 +0200

    changed id generator

--- src/Service/IdGenerator.php
+++ src/Service/IdGenerator.php
@@ -6,9 +6,7 @@ class IdGenerator
 {
     public static function generateIdAsInteger(): int
     {
-        $microtime = microtime(true); // Get current timestamp with microseconds
-        $random = mt_rand(100000, 999999); // Generate a random number
-        $stringToHash = $microtime . $random; // Combine timestamp and random number
-        return crc32($stringToHash);
+        // Generate a random number
+        return mt_rand(10000000, 99999999);
     }
 }
